b4300ddd1d119f37fd7cd8c267a7f3523c5ae4ba
jmalmolda <jmalmolda@gmail.com>
2017-07-18 22:50:33 +0100

    update calculator to use handlers

--- DecisionTechTest.Basket/PriceCalculator/Implementation/PriceCalculator.cs
+++ DecisionTechTest.Basket/PriceCalculator/Implementation/PriceCalculator.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DecisionTechTest.Basket.Model;
+using DecisionTechTest.Basket.OfferHandlers.Interface;
 using DecisionTechTest.Basket.PriceCalculator.Interface;
 using DecisionTechTest.Basket.Products.Interface;
 
@@ -10,9 +12,25 @@ namespace DecisionTechTest.Basket.PriceCalculator.Implementation
 {
     public class PriceCalculator : IPriceCalculator
     {
+        private readonly IOfferHandler _handler;
+
+        public PriceCalculator(IOfferHandler handler)
+        {
+            _handler = handler;
+        }
+
         public decimal CalculatePrice(IEnumerable<IProduct> products)
         {
-            return products?.Sum(product => product.Cost) ?? 0M;
+            List<ProductProcessedCost> processedProducts = products
+                .Select(product => new ProductProcessedCost
+                {               
+                    Cost = product.Cost,
+                    IsProcessed = false
+                }).ToList();
+
+            processedProducts = _handler.ApplyOffer(processedProducts);
+
+            return processedProducts?.Sum(product => product.Cost) ?? 0M;
         }
     }
 }
