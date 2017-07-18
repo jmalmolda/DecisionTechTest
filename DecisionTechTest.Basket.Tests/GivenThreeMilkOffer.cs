b2168568e9004b0730ab88983ba9f943fe96bd13
jmalmolda <jmalmolda@gmail.com>
2017-07-19 00:12:31 +0100

    implement offer

--- DecisionTechTest.Basket.Tests/GivenThreeMilkOffer.cs
+++ DecisionTechTest.Basket.Tests/GivenThreeMilkOffer.cs
@@ -32,7 +32,7 @@ namespace DecisionTechTest.Basket.Tests
             };
 
             // create system under test
-            TwoButterOffer offer = new TwoButterOffer();
+            ThreeMilkOffer offer = new ThreeMilkOffer();
 
             // execute test
             List<ProductProcessedCost> result = offer.ApplyOffer(products);
@@ -63,21 +63,72 @@ namespace DecisionTechTest.Basket.Tests
             };
 
             // create system under test
-            TwoButterOffer offer = new TwoButterOffer();
+            ThreeMilkOffer offer = new ThreeMilkOffer();
 
             // execute test
             List<ProductProcessedCost> result = offer.ApplyOffer(products);
 
             // assert the result
             result.Count.Should().Equal(4);
-            result[0].Product.Cost.Should().Equal(1.15M);
+            result[0].Product.Cost.Should().Equal(0M);
             result[0].IsProcessed.Should().Be.True();
             result[1].Product.Cost.Should().Equal(1.15M);
             result[1].IsProcessed.Should().Be.True();
             result[2].Product.Cost.Should().Equal(1.15M);
             result[2].IsProcessed.Should().Be.True();
-            result[2].Product.Cost.Should().Equal(0);
+            result[3].Product.Cost.Should().Equal(1.15M);
+            result[3].IsProcessed.Should().Be.True();
+        }
+
+        [TestMethod]
+        public void WhenApplyOfferWith8Milk_ThenReturnModified()
+        {
+            Milk product1 = new Milk();
+            Milk product2 = new Milk();
+            Milk product3 = new Milk();
+            Milk product4 = new Milk();
+            Milk product5 = new Milk();
+            Milk product6 = new Milk();
+            Milk product7 = new Milk();
+            Milk product8 = new Milk();
+
+            // create dependencies
+            var products = new List<ProductProcessedCost>
+            {
+                new ProductProcessedCost {Product = product1, IsProcessed = false},
+                new ProductProcessedCost {Product = product2, IsProcessed = false},
+                new ProductProcessedCost {Product = product3, IsProcessed = false},
+                new ProductProcessedCost {Product = product4, IsProcessed = false},
+                new ProductProcessedCost {Product = product5, IsProcessed = false},
+                new ProductProcessedCost {Product = product6, IsProcessed = false},
+                new ProductProcessedCost {Product = product7, IsProcessed = false},
+                new ProductProcessedCost {Product = product8, IsProcessed = false},
+            };
+
+            // create system under test
+            ThreeMilkOffer offer = new ThreeMilkOffer();
+
+            // execute test
+            List<ProductProcessedCost> result = offer.ApplyOffer(products);
+
+            // assert the result
+            result.Count.Should().Equal(8);
+            result[0].Product.Cost.Should().Equal(0M);
+            result[0].IsProcessed.Should().Be.True();
+            result[1].Product.Cost.Should().Equal(1.15M);
+            result[1].IsProcessed.Should().Be.True();
+            result[2].Product.Cost.Should().Equal(1.15M);
             result[2].IsProcessed.Should().Be.True();
+            result[3].Product.Cost.Should().Equal(1.15M);
+            result[3].IsProcessed.Should().Be.True();
+            result[4].Product.Cost.Should().Equal(0M);
+            result[4].IsProcessed.Should().Be.True();
+            result[5].Product.Cost.Should().Equal(1.15M);
+            result[5].IsProcessed.Should().Be.True();
+            result[6].Product.Cost.Should().Equal(1.15M);
+            result[6].IsProcessed.Should().Be.True();
+            result[7].Product.Cost.Should().Equal(1.15M);
+            result[7].IsProcessed.Should().Be.True();
         }
 
     }
--- DecisionTechTest.Basket/OfferHandlers/Implementation/ThreeMilkOffer.cs
+++ DecisionTechTest.Basket/OfferHandlers/Implementation/ThreeMilkOffer.cs
@@ -12,7 +12,27 @@ namespace DecisionTechTest.Basket.OfferHandlers.Implementation
     {
         public override List<ProductProcessedCost> ApplyOffer(List<ProductProcessedCost> products)
         {
-            throw new NotImplementedException();
+            for (int index = 0; index < products.Count; index++)
+            {
+                var product = products[index];
+                // check if we can apply the offer
+                if (product.Product is Milk 
+                    && !product.IsProcessed
+                    && products.Count(p => p.Product is Milk && !p.IsProcessed) > 3)
+                {
+                    product.Product.Cost = 0M;
+                    product.IsProcessed = true;
+                    // modify the products of the list relevant to the offer
+                    var usedProductIndex = products.FindIndex(p => p.Product is Milk && !p.IsProcessed);
+                    products[usedProductIndex].IsProcessed = true;
+                    usedProductIndex = products.FindIndex(p => p.Product is Milk && !p.IsProcessed);
+                    products[usedProductIndex].IsProcessed = true;
+                    usedProductIndex = products.FindIndex(p => p.Product is Milk && !p.IsProcessed);
+                    products[usedProductIndex].IsProcessed = true;
+                }
+            }
+
+            return products;
         }
     }
 }
