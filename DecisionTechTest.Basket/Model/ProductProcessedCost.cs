5c316e9e3d4c539b769861610b5fa4825eee3ee7
jmalmolda <jmalmolda@gmail.com>
2017-07-18 22:31:48 +0100

    add offer handler interface and processed products model

---
+++ DecisionTechTest.Basket/Model/ProductProcessedCost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTechTest.Basket.Model
+{
+    public class ProductProcessedCost
+    {
+        public decimal Cost { get; set; }
+        public bool IsProcessed { get; set; }
+    }
+}
---
+++ DecisionTechTest.Basket/OfferHandlers/Interface/IOfferHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DecisionTechTest.Basket.Model;
+
+namespace DecisionTechTest.Basket.OfferHandlers.Interface
+{
+    public interface IOfferHandler
+    {
+        List<ProductProcessedCost> ApplyOffer(List<ProductProcessedCost> products);
+    }
+}
