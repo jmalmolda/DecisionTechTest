b011eb89672482c217c0f120b73b885aa5dcc8b0
jmalmolda <jmalmolda@gmail.com>
2017-07-18 20:52:54 +0100

    update scenario

--- DecisionTechTest.Basket.Spec/BasketSteps.cs
+++ DecisionTechTest.Basket.Spec/BasketSteps.cs
@@ -8,37 +8,37 @@ namespace DecisionTechTest.Basket.Spec
     [Binding]
     public class BasketSteps
     {
-        private Basket basket = new Basket(new PriceCalculator.Implementation.PriceCalculator());
-        private decimal totalPrice = 0.0M;
+        private readonly Basket _basket = new Basket(new PriceCalculator.Implementation.PriceCalculator());
+        private decimal _totalPrice = 0.0M;
         [Given(@"I have added a bread product to the basket")]
         public void GivenIHaveAddedABreadProductToTheBasket()
         {
-            basket.AddProduct(new Bread());
+            _basket.AddProduct(new Bread());
         }
         
         [Given(@"I have added a butter product to the basket")]
         public void GivenIHaveAddedAButterProductToTheBasket()
         {
-            basket.AddProduct(new Butter());
+            _basket.AddProduct(new Butter());
         }
 
         [Given(@"I have added a milk product to the basket")]
         public void GivenIHaveAddedAMilkProductToTheBasket()
         {
-            basket.AddProduct(new Milk());
+            _basket.AddProduct(new Milk());
 
         }
 
         [When(@"I get the total price")]
         public void WhenIGetTheTotalPrice()
         {
-            totalPrice = basket.GetTotalPrice();
+            _totalPrice = _basket.GetTotalPrice();
         }
         
         [Then(@"the result should be £(.*)")]
         public void ThenTheResultShouldBe(Decimal p0)
         {
-            Assert.AreEqual(totalPrice, p0);
+            Assert.AreEqual(_totalPrice, p0);
         }
     }
 }
